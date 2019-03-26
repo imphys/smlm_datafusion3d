69df8b21636e73be52cbfff679b9381029d3f92f
Adrian Przybylski <adrian.przybylski@mpibpc.mpg.de>
2019-03-26 18:29:24 +0100

    Update fuse_particles_3d.h
    - fixed c++ linking
    - prevented multiple function definition

--- c_interface/fuse_particles_3d.h
+++ c_interface/fuse_particles_3d.h
@@ -1,22 +1,34 @@
+#ifndef FUSE_PARTICLES_3D
+#define FUSE_PARTICLES_3D
 
 #include <string.h>
 
-int fuse_particles_3d_portable(int argc, const char **argv);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-int fuse_particles_3d(
-    double * transformed_coordinates_x,
-    double * transformed_coordinates_y,
-    double * transformed_coordinates_z,
-    double * transformation_parameters,
-    int n_particles,
-    int * n_localizations_per_particle,
-    double * coordinates_x,
-    double * coordinates_y,
-    double * coordinates_z,
-    double * weights_xy,
-    double * weights_z,
-    int * channel_ids,
-    int averaging_channel_id,
-    int n_iterations_alltoall,
-    int n_iterations_onetoall,
-    int symmetry_order);
+    int fuse_particles_3d_portable(int argc, const char **argv);
+
+    int fuse_particles_3d(
+        double * transformed_coordinates_x,
+        double * transformed_coordinates_y,
+        double * transformed_coordinates_z,
+        double * transformation_parameters,
+        int n_particles,
+        int * n_localizations_per_particle,
+        double * coordinates_x,
+        double * coordinates_y,
+        double * coordinates_z,
+        double * weights_xy,
+        double * weights_z,
+        int * channel_ids,
+        int averaging_channel_id,
+        int n_iterations_alltoall,
+        int n_iterations_onetoall,
+        int symmetry_order);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // !FUSE_PARTICLES_3D
