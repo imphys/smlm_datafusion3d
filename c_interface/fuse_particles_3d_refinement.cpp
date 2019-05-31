992de7875436d45cc841aa0783c96d0ef5ccabb7
Adrian Przybylski <adrian.przybylski@mpibpc.mpg.de>
2019-05-31 14:31:18 +0200

    addendum

--- c_interface/fuse_particles_3d_refinement.cpp
+++ c_interface/fuse_particles_3d_refinement.cpp
@@ -15,15 +15,14 @@ int fuse_particles_3d_refinement_(int argc, const char **argv)
     double * transformed_coordinates_x = (double *)argv[0];
     double * transformed_coordinates_y = (double *)argv[1];
     double * transformed_coordinates_z = (double *)argv[2];
-    int32_t n_particles = *(int32_t *)argv[3];
-    int32_t * n_localizations_per_particle = (int32_t *)argv[4];
-    double * registration_matrix = (double *)argv[5];
-    double * coordinates_x = (double *)argv[6];
-    double * coordinates_y = (double *)argv[7];
-    double * coordinates_z = (double *)argv[8];
-    double transformation_refinement_threshold = *(double *)argv[9];
-    int32_t * channel_ids = (int *)argv[10];
-    int32_t averaging_channel_id = *(int32_t *)argv[11];
+    double * transformation_parameters = (double *)argv[3];
+    int32_t n_particles = *(int32_t *)argv[4];
+    int32_t * n_localizations_per_particle = (int32_t *)argv[5];
+    double * registration_matrix = (double *)argv[6];
+    double * coordinates_x = (double *)argv[7];
+    double * coordinates_y = (double *)argv[8];
+    double * coordinates_z = (double *)argv[9];
+    double transformation_refinement_threshold = *(double *)argv[10];
 
     // total number of localizations
     size_t n_localizations = 0;
@@ -41,13 +40,12 @@ int fuse_particles_3d_refinement_(int argc, const char **argv)
     mxArray * mx_coordinates_y = mxCreateNumericMatrix(n_localizations, 1, mxDOUBLE_CLASS, mxREAL);
     mxArray * mx_coordinates_z = mxCreateNumericMatrix(n_localizations, 1, mxDOUBLE_CLASS, mxREAL);
     mxArray * mx_transformation_refinement_threshold = mxCreateNumericMatrix(1, 1, mxDOUBLE_CLASS, mxREAL);
-    mxArray * mx_channel_ids = mxCreateNumericMatrix(n_localizations, 1, mxINT32_CLASS, mxREAL);
-    mxArray * mx_averaging_channel_id = mxCreateNumericMatrix(1, 1, mxINT32_CLASS, mxREAL);
 
     // output
     mxArray * mx_transformed_coordinates_x = NULL;
     mxArray * mx_transformed_coordinates_y = NULL;
     mxArray * mx_transformed_coordinates_z = NULL;
+    mxArray * mx_transformation_parameters = NULL;
 
     // copy input
     memcpy(mxGetPr(mx_n_particles), &n_particles, sizeof(int32_t));
@@ -57,8 +55,6 @@ int fuse_particles_3d_refinement_(int argc, const char **argv)
     memcpy(mxGetPr(mx_coordinates_y), coordinates_y, n_localizations * sizeof(double));
     memcpy(mxGetPr(mx_coordinates_z), coordinates_z, n_localizations * sizeof(double));
     memcpy(mxGetPr(mx_transformation_refinement_threshold), &transformation_refinement_threshold, sizeof(double));
-    memcpy(mxGetPr(mx_channel_ids), channel_ids, n_localizations * sizeof(int32_t));
-    memcpy(mxGetPr(mx_averaging_channel_id), &averaging_channel_id, sizeof(int32_t));
 
     // initialize application
 
@@ -75,19 +71,18 @@ int fuse_particles_3d_refinement_(int argc, const char **argv)
 
     // run application
     mlfMcc_fuse_particles_3d_refinement(
-        3,
+        4,
         &mx_transformed_coordinates_x,
         &mx_transformed_coordinates_y,
         &mx_transformed_coordinates_z,
+        &mx_transformation_parameters,
         mx_n_particles,
         mx_n_localizations_per_particle,
         mx_registration_matrix,
         mx_coordinates_x,
         mx_coordinates_y,
         mx_coordinates_z,
-        mx_transformation_refinement_threshold,
-        mx_channel_ids,
-        mx_averaging_channel_id);
+        mx_transformation_refinement_threshold);
 
     if (mx_transformed_coordinates_x == NULL ||
         mx_transformed_coordinates_y == NULL ||
@@ -100,12 +95,14 @@ int fuse_particles_3d_refinement_(int argc, const char **argv)
     memcpy(transformed_coordinates_x, mxGetPr(mx_transformed_coordinates_x), n_localizations * sizeof(double));
     memcpy(transformed_coordinates_y, mxGetPr(mx_transformed_coordinates_y), n_localizations * sizeof(double));
     memcpy(transformed_coordinates_z, mxGetPr(mx_transformed_coordinates_z), n_localizations * sizeof(double));
+    memcpy(transformation_parameters, mxGetPr(mx_transformation_parameters), 16 * n_particles * sizeof(double));
 
     // mcc_fuse_particles_3dTerminate();
 
     mxDestroyArray(mx_transformed_coordinates_x);
     mxDestroyArray(mx_transformed_coordinates_y);
     mxDestroyArray(mx_transformed_coordinates_z);
+    mxDestroyArray(mx_transformation_parameters);
 
     mxDestroyArray(mx_n_particles);
     mxDestroyArray(mx_n_localizations_per_particle);
@@ -114,8 +111,6 @@ int fuse_particles_3d_refinement_(int argc, const char **argv)
     mxDestroyArray(mx_coordinates_y);
     mxDestroyArray(mx_coordinates_z);
     mxDestroyArray(mx_transformation_refinement_threshold);
-    mxDestroyArray(mx_channel_ids);
-    mxDestroyArray(mx_averaging_channel_id);
 
     return 0;
 }
@@ -128,15 +123,14 @@ int fuse_particles_3d_refinement_portable(int argc, void *argv[])
         (double *)argv[0],
         (double *)argv[1],
         (double *)argv[2],
-        *(int32_t *)argv[3],
-        (int32_t *)argv[4],
-        (double *)argv[5],
+        (double *)argv[3],
+        *(int32_t *)argv[4],
+        (int32_t *)argv[5],
         (double *)argv[6],
         (double *)argv[7],
         (double *)argv[8],
-        *(double *)argv[9],
-        (int32_t *)argv[10],
-        *(int32_t *)argv[11]);
+        (double *)argv[9],
+        *(double *)argv[10]);
 
 }
 
@@ -145,33 +139,31 @@ int fuse_particles_3d_refinement(
     double * transformed_coordinates_x,
     double * transformed_coordinates_y,
     double * transformed_coordinates_z,
+    double * transformation_parameters,
     int32_t n_particles,
     int32_t * n_localizations_per_particle,
     double * registration_matrix,
     double * coordinates_x,
     double * coordinates_y,
     double * coordinates_z,
-    double transformation_refinement_threshold,
-    int32_t * channel_ids,
-    int32_t averaging_channel_id)
+    double transformation_refinement_threshold)
 {
     LOAD_MCC_LIBRARY
 
-    const int argc = 12;
+    const int argc = 11;
     const char * argv[argc];
 
     argv[0] = (char *)transformed_coordinates_x;
     argv[1] = (char *)transformed_coordinates_y;
     argv[2] = (char *)transformed_coordinates_z;
-    argv[3] = (char *)(&n_particles);
-    argv[4] = (char *)n_localizations_per_particle;
-    argv[5] = (char *)registration_matrix;
-    argv[6] = (char *)coordinates_x;
-    argv[7] = (char *)coordinates_y;
-    argv[8] = (char *)coordinates_z;
-    argv[9] = (char *)(&transformation_refinement_threshold);
-    argv[10] = (char *)channel_ids;
-    argv[11] = (char *)(&averaging_channel_id);
+    argv[3] = (char *)transformation_parameters;
+    argv[4] = (char *)(&n_particles);
+    argv[5] = (char *)n_localizations_per_particle;
+    argv[6] = (char *)registration_matrix;
+    argv[7] = (char *)coordinates_x;
+    argv[8] = (char *)coordinates_y;
+    argv[9] = (char *)coordinates_z;
+    argv[10] = (char *)(&transformation_refinement_threshold);
 
     // initialize application
     if (!mcr_start())
