8750c28ac9e3ab7200d200070b9ca55df5427353
hheydarian <hheydarian@elras-Precision-7920-Tower>
2019-09-26 18:50:40 +0200

    function name changed from all2all3Dn to pairFitting

--- MATLAB/all2all3D/all2all3Dn.m
+++ MATLAB/all2all3D/all2all3Dn.m
@@ -1,4 +1,35 @@
-function param = all2all3Dn(ptc1, ptc2, sig1, sig2, scale, nIteration, USE_GPU_GAUSSTRANSFORM, USE_GPU_EXPDIST)
+% all2all3Dn   computes the Gauss transform between point sets A and B
+% with the scale parameter scale
+%
+% SYNOPSIS:
+%   [f, grad] = GaussTransform(A, B, scale)
+%
+% INPUT
+%   A
+%       The first point set
+%
+%   B
+%       The second point set
+%
+% OUTPUT
+%   f 
+%       The gausstransform output. 
+%
+%   g
+%       The gradient vector
+%
+% NOTES
+%       The inner product between two spherical Gaussian mixtures computed 
+%       using the Gauss Transform. The centers of the two mixtures are 
+%       given in terms of two point sets A and B (of same dimension d)
+%       represented by an mxd matrix and an nxd matrix, respectively.
+%       It is assumed that all the components have the same covariance 
+%       matrix represented by a scale parameter (scale).  Also, in each 
+%       mixture, all the components are equally weighted.
+%
+% Author: Hamidreza Heydarian, 2019 
+
+function param = pairFitting3D(ptc1, ptc2, sig1, sig2, scale, nIteration, USE_GPU_GAUSSTRANSFORM, USE_GPU_EXPDIST)
 % ptc1  point cloud of particle 1
 % ptc2  point cloud of particle 2
 % sig1  uncertainties for points in ptc1
