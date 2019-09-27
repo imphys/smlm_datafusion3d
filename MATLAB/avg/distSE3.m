a81cc88bb2b70a19e996a3fed807584f94f9811b
hheydarian <hheydarian@elras-Precision-7920-Tower>
2019-09-27 17:48:12 +0200

    commets are added

--- MATLAB/avg/distSE3.m
+++ MATLAB/avg/distSE3.m
@@ -1,37 +1,38 @@
+% distSE3  compute the geodesic distance between two transformation matrix
+% (SE3)
+%
+% SYNOPSIS:
+%   d = distSE3(X,Y)
+%
+% INPUT
+%   X  
+%       transformation matrix 1 (4x4)
+%   Y  
+%       transformation matrix 2 (4x4)
+%
+% OUTPUT
+%   d       
+%       the geodesic distance
+%    
+% NOTE
+%       reference: Park, F. C. Distance Metrics on the Rigid-Body Motions 
+%       with Applications to Mechanism Design. Journal of Mechanical Design 117, 48 (1995).
+%
+% (C) Copyright 2019               Quantitative Imaging Group
+%     All rights reserved          Faculty of Applied Physics
+%                                  Delft University of Technology
+%                                  Lorentzweg 1
+%                                  2628 CJ Delft
+%                                  The Netherlands
+%
+% Author: Hamidreza Heydarian, 2019 
+
 function d = distSE3(X,Y)
 
-%DISTXY Custom Distance metric for the JHU kinematics data
-% 1-3    (3) : tooltip xyz                    
-% 4-12   (9) : tooltip R    
-% 13-15  (3) : tooltip trans vel x', y', z'   
-% 16-18  (3) : tooltip rot_vel                
-% 19     (1) : gripper angle   
-% We use the scale dependent left-invariant metric from 
-% "Distance Metrics on the Rigid Body Rotations with Applications 
-% to Mechanism Design "
+    % rotation matrix similarity
+    R1 = X(1:3,1:3);
+    R2 = Y(1:3,1:3);
 
-if nargin <3
-    w = [1, 2]; %this works under appropriate scaling of positions
-else 
-    w = varargin{1};
-end
-    
-% euclidiean distance
-% d_trans = norm(X(1:3,4)- Y(1:3,4),2); 
+    d = sqrt(trace(logm(R1'*R2)'*logm(R1'*R2))/2);
 
-% rotation matrix similarity
-R1 = X(1:3,1:3);
-R2 = Y(1:3,1:3);
-% d_rot = 0.5*trace(logm(R1'*R2));
-% d_rot = norm(logm(R1'*R2),'fro');
-% d_rot = trace(logm(R1'*R2)'*logm(R1'*R2));
-d_rot = sqrt(trace(logm(R1'*R2)'*logm(R1'*R2))/2);
-%quaternion Distance
-% qx = dcm2quat(R1);
-% qy = dcm2quat(R2);
-% 
-% d_quat = 1 - abs(qx.*qy); %[0,1] measure of similarity
-
-% d = w(1)*d_trans + w(2)*d_rot;
-d = d_rot;
 end
