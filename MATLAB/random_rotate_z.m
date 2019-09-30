ecd7378c0515ad6504c6b124f31c2668ae59a912
hheydarian <hheydarian@elras-Precision-7920-Tower>
2019-09-30 17:23:16 +0200

    unnecessary file is removed

--- MATLAB/random_rotate_z.m
+++
@@ -1,9 +0,0 @@
-function pointset_out = random_rotate_z(pointset_in, step_size)
-
-symmetry_order = 2 * pi / step_size;
-
-angle = step_size * floor(symmetry_order * rand);
-r = rotz(rad2deg(angle));
-pointset_out = pointset_in * r';
-
-end
