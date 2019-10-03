c17587d82a7ca6dd8a609052e771608fe649fd64
hrheydarian <hheydarian@elras-Precision-7920-Tower>
2019-10-03 18:48:51 +0200

    invTransform implemented

--- MATLAB/auxiliary/invTransform.m
+++ MATLAB/auxiliary/invTransform.m
@@ -1,3 +1,28 @@
+% invTransform      invert geometric transformation 
+%
+% SYNOPSIS:
+%   invT = invTransform(estA)
+%
+% INPUT
+%   estA
+%       the 4x4 transformations in homogeneous coordinate
+%
+% OUTPUT
+%   invT
+%       the initial aligned particles
+%
+% (C) Copyright 2019               Quantitative Imaging Group
+%     All rights reserved          Faculty of Applied Physics
+%                                  Delft University of Technology
+%                                  Lorentzweg 1
+%                                  2628 CJ Delft
+%                                  The Netherlands
+%
+% Hamidreza Heydarian, 2019
+
 function invT = invTransform(estA)
 
+    invT = inv(estA);
+    invT(:,end) = [0;0;0;1];
+    
 end
