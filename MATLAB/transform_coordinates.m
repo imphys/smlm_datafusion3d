11ec55a1cf3ba6bc455779c7f789e67eccaf3be4
Ronald Ligteringen <r.ligteringen@gmail.com>
2021-03-31 11:31:57 +0200

    Delete all the things.

--- MATLAB/transform_coordinates.m
+++
@@ -1,6 +0,0 @@
-function transformed_coordinates = transform_coordinates(coordinates, rotation, shift)
-    
-shift = repmat(shift, size(coordinates,1),1);
-transformed_coordinates =  coordinates * rotation + shift;
-
-end
